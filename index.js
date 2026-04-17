52ddaa908ca071d74dc05d88434811309d9b3736
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-04-17 13:32:25 +0300

    feat:search US-states alerts

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 // index.js
 const weatherApi = "https://api.weather.gov/alerts/active?area="
+const STATE_ABBR=document.querySelector('#state-input').value
 
 // Your code here!
+
+function displayAlerts(data){
+ const alerts=document.querySelector('#alerts-display') 
+ const summary=document.createElement('p') 
+ summary.textContent=`${data.title}:${data.features.length}`
+ alerts.append(summary)
+ const features=data.features
+ features.forEach(feature => {
+        const headlineList=document.createElement('li') 
+headlineList.textContent= `${feature.properties.headline}`
+ alerts.append(headlineList)
+});
+}
+
+async function fetchWeatherAlerts(STATE_ABBR){
+try{const responce=await fetch(`${weatherApi}${STATE_ABBR}`)
+const data=await responce.json()
+displayAlerts(data)
+
+}catch(error){
+    console.error('Error in fetching alerts:',error)
+}
+}
+const form=document.querySelector(".selectCountry")
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const STATE_ABBR=document.querySelector('#state-input').value
+fetchWeatherAlerts(STATE_ABBR)
+
+})
+
+
+
+
+
+
+// const form=document.querySelector('form')
+// form.addEventListener('submit',(event)=>{
+// event.preventDefault()
+// const stateInput=document.querySelector('#state-input').value
+
+
+// const stateInput=event.target.state-input.value
+// })
+
